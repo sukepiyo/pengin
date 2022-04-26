5c62d706e80e1f09f38f5a73e6f8ef3c05a07a89
sukepiyo <keisukekudo0828@gmail.com>
2022-04-26 14:59:39 +0900

    add jQuery part.

---
+++ practice-01/main.js
@@ -0,0 +1,13 @@
+$('.nav-btn').click(function(){
+  $('.bar').toggleClass('close');
+  $('.header-nav').fadeToggle(500);
+  $('body').toggleClass('noscroll');
+});
+
+if ($(window).width() < 768) {
+  $('.nav-item').click(function(){
+    $('.header-nav').fadeOut(500);
+    $('.bar').removeClass('.close');
+    $('body').removeClass('noscroll');
+  });
+}
